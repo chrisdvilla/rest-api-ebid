6fdf3810d3d93562618f0420f31d845dbdc40f71
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-04-07 23:36:26 -0400

    add cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import app  from './app.js'
 import {PORT}  from './config.js'
 
+import cors from 'cors'
+
+app.use(cors({origin: '*'}));
 app.listen(PORT)
 console.log('Server running on port', PORT)
