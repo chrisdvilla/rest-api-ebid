d2454f58e06e5f0b2d3f58b00b6378bb896f98d2
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-05-23 10:18:35 -0400

    cors prueba 3

--- src/index.js
+++ src/index.js
@@ -9,15 +9,27 @@ const corsOptions = {
   };
  
 //app.use(cors(corsOptions));
-app.use(cors());
+//app.use(cors());
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
 
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   next()
 
+}); */
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+      res.send(200);
+  } else {
+      next();
+  }
 });
 
 app.listen(PORT)
