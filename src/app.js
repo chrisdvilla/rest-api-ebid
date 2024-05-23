d6a7af30399d32da9d3023fca631dc824b137a70
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-05-23 10:29:07 -0400

    cors prueba 5

--- src/app.js
+++ src/app.js
@@ -13,14 +13,14 @@ const cors = require('cors');
 const app = express()
 app.use(cors());
 
-app.use((req, res, next) => {
-
-      res.append('Access-Control-Allow-Origin', ['*']);
-      res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-      res.append('Access-Control-Allow-Headers', 'Content-Type');
-      next()
-
-   });
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+  if(req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+      return res.status(200).json({});
+  }
+  next();
+});
 
 
 app.use(express.json())
