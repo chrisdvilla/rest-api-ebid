9c03797fec011f9c54cade23e539243992a7c017
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-05-23 10:37:31 -0400

    cors prueba 7

--- src/app.js
+++ src/app.js
@@ -12,14 +12,14 @@ const cors = require('cors');
 
 const app = express();
 
-const corsOptions ={
+/* const corsOptions ={
    origin:'*', 
    credentials:true,            //access-control-allow-credentials:true
    optionSuccessStatus:200,
    methods: "GET, POST"
  }
  
- app.use(cors(corsOptions));
+ app.use(cors(corsOptions)); */
 
 
 app.use(express.json())
--- src/index.js
+++ src/index.js
@@ -9,17 +9,7 @@ const corsOptions = {
   };
  
 //app.use(cors(corsOptions));
-//app.use(cors());
 
-
-app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
-  if(req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-      return res.status(200).json({});
-  }
-  next();
-});
 
 app.listen(PORT)
 console.log('Server running on port', PORT)
