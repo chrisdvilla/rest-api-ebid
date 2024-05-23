4dcd0ada1838fdfd45d984e4c5f99e78ba34656c
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-05-23 09:41:43 -0400

    cors prueba Fatz2

--- src/index.js
+++ src/index.js
@@ -11,5 +11,14 @@ const corsOptions = {
 //app.use(cors(corsOptions));
 app.use(cors());
 
+app.use((req, res, next) => {
+
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next()
+
+});
+
 app.listen(PORT)
 console.log('Server running on port', PORT)
