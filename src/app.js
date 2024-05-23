413d923830da88946e18cf073a8ae7fb8eb3576d
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-05-23 09:37:21 -0400

    cors prueba Fatz

--- src/app.js
+++ src/app.js
@@ -13,6 +13,15 @@ const cors = require('cors');
 const app = express()
 app.use(cors());
 
+app.use((req, res, next) => {
+
+      res.append('Access-Control-Allow-Origin', ['*']);
+      res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.append('Access-Control-Allow-Headers', 'Content-Type');
+      next()
+
+   });
+
 
 app.use(express.json())
 
