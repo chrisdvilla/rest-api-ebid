2e30b69407828e1496b9ecf9125d6381988dde81
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-05-23 22:07:10 -0400

    cors metodo Patch agregado

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.all('*', function(req, res, next) {
    res.header('Access-Control-Allow-Origin', '*');
    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, PATCH');
 
    if (req.method == 'OPTIONS') {
        res.send(200);
