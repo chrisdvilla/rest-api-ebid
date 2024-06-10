9eb04fdcf4a0afca6d46137f0ff0ca32e126dd83
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-06-10 01:04:09 -0400

    materiasDoc fixed

--- src/controllers/materiasDoc.controller.js
+++ src/controllers/materiasDoc.controller.js
@@ -4,7 +4,7 @@ export const getMaterias = async (req, res) => {
     console.log(req.params);
     console.log(req.body)
    try {
-    const [rows] = await pool.query("SELECT * FROM paralelo JOIN curso ON paralelo.id_materia = curso.id JOIN horario ON horario.id_set = paralelo.id WHERE paralelo.docente = ?", [
+    const [rows] = await pool.query("SELECT * FROM paralelo JOIN curso ON paralelo.id_materia = curso.id JOIN horario ON horario.id_set = paralelo.id WHERE paralelo.docente = ? AND ", [
         req.params.docente,
       ]);
     res.json(rows);
@@ -20,8 +20,8 @@ export const getAllMaterias = async (req, res) => {
     console.log("materias");
     console.log(req.body)
    try {
-    //const [rows] = await pool.query("SELECT * FROM paralelo JOIN curso ON paralelo.id_materia = curso.id JOIN horario ON horario.id_set = paralelo.id");
-    const [rows] = await pool.query("SELECT * FROM paralelo JOIN curso ON paralelo.id_materia = curso.id JOIN horario ");
+    const [rows] = await pool.query("SELECT * FROM paralelo JOIN curso ON paralelo.id_materia = curso.id JOIN horario ON horario.id_set = paralelo.id");
+    //const [rows] = await pool.query("SELECT * FROM paralelo JOIN curso ON paralelo.id_materia = curso.id JOIN horario ");
     res.json(rows);
   } catch (error) {
     return res.status(500).json({
