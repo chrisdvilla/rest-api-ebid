2db24aebd616aea0cec45f3f824bdb66397596d5
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-06-14 11:55:06 -0400

    notas std

--- src/controllers/alumnosMaterias.controller.js
+++ src/controllers/alumnosMaterias.controller.js
@@ -3,7 +3,7 @@ import { pool } from "../db.js";
 export const getAlumnoMat = async (req, res) => {
     console.log(req.params);
    try {
-     const [rows] = await pool.query("SELECT DISTINCT nombre, apellido_paterno, apellido_materno, num_registro, id_alumno, id_materia FROM alumno JOIN materias_reg ON materias_reg.id_alumno = alumno.id  WHERE materias_reg.id_materia = ? ", [
+     const [rows] = await pool.query("SELECT DISTINCT nombre, apellido_paterno, apellido_materno, num_registro, id_alumno, id_materia, materias_reg.codigo_mat FROM alumno JOIN materias_reg ON materias_reg.id_alumno = alumno.id  WHERE materias_reg.id_materia = ? ", [
         req.params.id_materia,
       ]); 
     res.json(rows);
--- src/routes/calificaciones.routes.js
+++ src/routes/calificaciones.routes.js
@@ -1,11 +1,13 @@
 import {Router} from 'express'
-import {getCalificaciones, getCalificacion, createCalificacion, updateCalificacion} from '../controllers/calificaciones.controller.js'
+import {getCalificaciones, getCalificacion, createCalificacion, updateCalificacion, getCalificacionAlumno} from '../controllers/calificaciones.controller.js'
 
 const router = Router()
 
 router.get('/calificaciones', getCalificaciones);
 
 router.get('/calificaciones/:id_alumno/:id_materia', getCalificacion);
+
+router.get('/calificaciones/:id_alumno/', getCalificacionAlumno);
 
 router.post('/calificaciones', createCalificacion);
 
