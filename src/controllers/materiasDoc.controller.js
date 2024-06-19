4ed7312fe9182afa9bb40b27432002dc2c652747
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-06-19 00:51:11 -0400

    select horarios

--- src/controllers/materiasDoc.controller.js
+++ src/controllers/materiasDoc.controller.js
@@ -32,7 +32,7 @@ export const getAllMaterias = async (req, res) => {
 export const getMateriasCarrera = async (req, res) => {
   console.log(req.params);
  try {
-  const [rows] = await pool.query("SELECT DISTINCT codigo_mat, paralelo, id_materia, predecesor_mat, nombre_mat, id_docente, paralelo.id, detalle_mat, docente, paralelo.gestion  FROM paralelo JOIN curso ON paralelo.id_materia = curso.id JOIN horario ON horario.id_set = paralelo.id JOIN carreras_mat ON carreras_mat.id_set = paralelo.id WHERE carreras_mat.carrera = ?", [
+  const [rows] = await pool.query("SELECT DISTINCT codigo_mat, paralelo, id_materia, predecesor_mat, nombre_mat, id_docente, paralelo.id, detalle_mat, docente, paralelo.gestion, horario.id_set  FROM paralelo JOIN curso ON paralelo.id_materia = curso.id JOIN horario ON horario.id_set = paralelo.id JOIN carreras_mat ON carreras_mat.id_set = paralelo.id WHERE carreras_mat.carrera = ?", [
     req.params.carrera,
     ]);
   res.json(rows);
@@ -43,3 +43,24 @@ export const getMateriasCarrera = async (req, res) => {
 } 
 
 };
+
+export const getMateriasHorarios = async (req, res) => {
+  console.log(req.params);
+  try {
+    const [rows] = await pool.query("SELECT * FROM horario WHERE id_set = ?", [
+      req.params.id_paralelo,
+    ]);
+
+    if (rows.length <= 0)
+      return res.status(404).json({
+        message: "Registro no encontrado",
+      });
+
+    res.json(rows);
+  } catch (error) {
+    return res.status(500).json({
+      //message: "Algo salio mal",
+      message: error.message,
+    });
+  }
+}; 
--- src/routes/materiasDoc.routes.js
+++ src/routes/materiasDoc.routes.js
@@ -1,11 +1,12 @@
 import {Router} from 'express'
-import {getMaterias, getAllMaterias, getMateriasCarrera } from '../controllers/materiasDoc.controller.js'
+import {getMaterias, getAllMaterias, getMateriasCarrera, getMateriasHorarios } from '../controllers/materiasDoc.controller.js'
 
 const router = Router()
 
 router.get('/materias/:id_docente', getMaterias);
 router.get('/materias/', getAllMaterias);
 router.get('/materiasCarrera/:carrera', getMateriasCarrera);
+router.get('/materiasHorarios/:id_paralelo', getMateriasHorarios);
 
 
 export default router
