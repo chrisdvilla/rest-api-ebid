e98aa2c5d95029d0462a17ac21f39ca820da09ab
Christian Delgado Villavicencio <christian.lnx@gmail.com>
2024-06-25 10:20:09 -0400

    habilitaciones fixed

--- src/controllers/configuracion.controller.js
+++ src/controllers/configuracion.controller.js
@@ -19,7 +19,11 @@ export const getHabilitacion = async (req, res) => {
     console.log(req.params);
   const { id } = req.params;
   const { 
-    apertura_materias, 
+    apertura_materias1, 
+    apertura_materias2, 
+    apertura_materias3, 
+    apertura_materias4, 
+    apertura_materias2T, 
     apertura_notas, 
     gestion
 
@@ -27,9 +31,13 @@ export const getHabilitacion = async (req, res) => {
 
        
   try {
-    const [result] = await pool.query('UPDATE configuracion SET apertura_materias = IFNULL(?,apertura_materias), apertura_notas = IFNULL(?,apertura_notas), gestion = IFNULL(?,gestion) WHERE id = ?'
+    const [result] = await pool.query('UPDATE configuracion SET apertura_materias1 = IFNULL(?,apertura_materias1), apertura_materias2 = IFNULL(?,apertura_materias2), apertura_materias3 = IFNULL(?,apertura_materias3), apertura_materias4 = IFNULL(?,apertura_materias4), apertura_materias2T = IFNULL(?,apertura_materias2T), apertura_notas = IFNULL(?,apertura_notas), gestion = IFNULL(?,gestion) WHERE id = ?'
   , [
-    apertura_materias, 
+    apertura_materias1, 
+    apertura_materias2, 
+    apertura_materias3, 
+    apertura_materias4, 
+    apertura_materias2T,
     apertura_notas, 
     gestion,
     id
@@ -47,7 +55,8 @@ export const getHabilitacion = async (req, res) => {
     res.json(rows[0]);
   } catch (error) {
     return res.status(500).json({
-      message: "Algo salio mal",
+      //message: "Algo salio mal",
+      message: error.message,
     });
   }
 }; 
